c3a8e10a99dfd7ce8f0bf0cc8b02e761f3d6fcc9
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-27 21:00:42 +0530

    01_basic_cui_calculator

---
+++ CLASS/SESSION_003/01_basic_cui_calculator.py
@@ -0,0 +1,18 @@
+n1 = int(input('Enter Number 1:'))
+n2 = int(input('Enter Number 2:'))
+operation = int(input('Press 1 for Addition, 2 for Substraction:'))
+
+if operation == 1:
+    result = n1 + n2
+    print('Result of Addition is:', result)
+
+elif operation == 2:
+    result = n1 - n2
+    print('Result of Substraction is:', result)
+
+else:
+    printf('Invalid Operation Code')
+
+
+
+
