06ce0d70a17f6ca0f33d38076b51228e064cd76b
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-28 13:13:39 +0530

    02_basicLogs.py

---
+++ CLASS/SESSION_007/02_basicsLogs.py
@@ -0,0 +1,16 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> m = 10
+>>> n = 11
+>>> addition = m + n
+>>> substraction = m - n
+>>> multiplication = m * n
+>>> division = m / n
+>>> print(addition)
+21
+>>> print(multiplication)
+110
+>>> print(division)
+0.9090909090909091
+>>> print(substraction)
+-1
