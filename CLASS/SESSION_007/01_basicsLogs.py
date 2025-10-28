2288de2397443c7e0f7d50878332032d97074755
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-28 13:01:07 +0530

    01_basicLogs

---
+++ CLASS/SESSION_007/01_basicsLogs.py
@@ -0,0 +1,23 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n = 10
+>>> print(n)
+10
+>>> type(n)
+<class 'int'>
+>>> id(n)
+140718461318344
+>>> f = 1.1
+>>> print(f)
+1.1
+>>> type(f)
+<class 'float'>
+>>> id(f)
+2346996192496
+>>> ch = 'A'
+>>> print(ch)
+A
+>>> type(ch)
+<class 'str'>
+>>> id(ch)
+140718461381088
