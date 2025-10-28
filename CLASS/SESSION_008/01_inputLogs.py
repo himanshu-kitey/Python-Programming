fe095e3670eb8f277bbecd7d4b76f41a3ad87dbe
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-28 14:41:36 +0530

    01_inputLogs.py

---
+++ CLASS/SESSION_008/01_inputLogs.py
@@ -0,0 +1,63 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print('Hello')
+Hello
+>>> print('Himanshu Kitey')
+Himanshu Kitey
+>>> print('I Love Python')
+I Love Python
+>>> n = 10
+>>> m = 20
+>>> addition = m + n
+>>> s = input('Enter Something:')
+Enter Something:Himanshu Kitey
+>>> print(s)
+Himanshu Kitey
+>>> type(s)
+<class 'str'>
+>>> id(s)
+1737815909040
+>>> ord(s)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    ord(s)
+TypeError: ord() expected a character, but string of length 14 found
+>>> ord('s')
+115
+>>> s1 = input('Enter a Number:')
+Enter a Number:12345
+>>> print(s1)
+12345
+>>> type(s1)
+<class 'str'>
+>>> id(s1)
+1737827933120
+>>> ord('1')
+49
+>>> ord('2')
+50
+>>> ord('3')
+51
+>>> ord('4')
+52
+ord('5')
+53
+bin(49)[2:]
+'110001'
+bin(50)[2:]
+'110010'
+bin(51)[2:]
+'110011'
+bin(52)[2:]
+'110100'
+bin(53)[2:]
+'110101'
+s = input('Enter a Number:')
+Enter a Number:345
+type(s)
+<class 'str'>
+s1 = int(s)
+type(s1)
+<class 'int'>
+type(s1)
+<class 'int'>
