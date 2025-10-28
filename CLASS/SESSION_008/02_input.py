d28441de24fd0ac7a1f77b1cca812d722802bcc3
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-28 16:48:16 +0530

    02_input.py

---
+++ CLASS/SESSION_008/02_input.py
@@ -0,0 +1,14 @@
+print('Input String Demo:')
+s1 = input('Enter a Message:')
+print('Entered String is:', s1)
+
+print('Input Integer Demo:')
+s2 = input('Enter an Integer:')
+number = int(s2);
+print('Entered Integer is:', number)
+
+print('Input Float Demo')
+s3 = input('Enter a Fractional Number:')
+f_number = float(s3)
+print('Entered Fractional Number is:', f_number)
+
