e8ac957ea9d439c53f0443cd906a44d5eadf9b50
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-28 14:07:36 +0530

    04_basicLogs.py

---
+++ CLASS/SESSION_007/04_basicLogs.py
@@ -0,0 +1,22 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> s = 'I Love You'
+>>> print(s)
+I Love You
+>>> type(s)
+<class 'str'>
+>>> bin(9)[2:]
+'1001'
+>>> bin(9)
+'0b1001'
+>>> bin(ord('9'))[2:]
+'111001'
+>>> 9/2
+4.5
+>>> 9//2
+4
+>>> '9'/2
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    '9'/2
+TypeError: unsupported operand type(s) for /: 'str' and 'int'
