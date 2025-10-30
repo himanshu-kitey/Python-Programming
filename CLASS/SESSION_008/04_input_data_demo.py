721da1658b32c52f5453e07ba8b532a35383702d
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-30 19:43:53 +0530

    04_input_data_demo.py

---
+++ CLASS/SESSION_008/04_input_data_demo.py
@@ -0,0 +1,13 @@
+print('Enter String Demo')
+s1 = input('Enter a String:')
+print('Entered String:', s1)
+
+print('Entered Integer Demo')
+s2 = input('Enter an Integer:')
+int_integer = int(s2)
+print('Entered Integer:', int_integer)
+
+print('Entered Float Demo')
+s3 = input('Enter an Float:')
+float_data = float(s3)
+print('Entered Float is:', float_data)
