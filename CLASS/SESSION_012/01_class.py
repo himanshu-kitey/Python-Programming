5825762f9fbbb967a17c8924a5f47be859586ada
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-10-31 12:32:00 +0530

    01_class.py

--- CLASS/SESSION_012/01_class.py
+++ CLASS/SESSION_012/01_class.py
@@ -1,35 +1,55 @@
 Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
->>> s = 'Himanshu'
->>> print(s)
+s = 'Himanshu'
+print(s)
 Himanshu
->>> type(s)
+type(s)
 <class 'str'>
->>> s1 = s.upper()
->>> print(s1)
+s1 = s.upper()
+print(s1)
 HIMANSHU
->>> print(s)
+print(s)
 Himanshu
->>> 
->>> u = 'PYTHON'
->>> print(u)
+
+u = 'PYTHON'
+print(u)
 PYTHON
->>> type(u)
+type(u)
 <class 'str'>
->>> u1 = u.lower()
->>> print(u)
+u1 = u.lower()
+print(u)
 PYTHON
->>> print(u1)
+print(u1)
 python
->>> 
->>> s = 'Himanshu:Sanjay:Kitey'
->>> L = s.split(':')
->>> print(s)
+
+s = 'Himanshu:Sanjay:Kitey'
+L = s.split(':')
+print(s)
 Himanshu:Sanjay:Kitey
->>> print(L)
+print(L)
 ['Himanshu', 'Sanjay', 'Kitey']
->>> 
->>> v = 'Himanshu:Sanjay:Kitey'
->>> L = str.split(v, ':')
->>> print(L)
+
+v = 'Himanshu:Sanjay:Kitey'
+L = str.split(v, ':')
+print(L)
 ['Himanshu', 'Sanjay', 'Kitey']
+
+>>> dir(str)
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+>>> dir(print)
+['__call__', '__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__name__', '__ne__', '__new__', '__qualname__', '__reduce__', '__reduce_ex__', '__repr__', '__self__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__text_signature__']
+>>> s = 'Himanshu'
+>>> b = s.title()
+>>> print(b)
+Himanshu
+>>> b = s.isupper()
+>>> print(b)
+False
+>>> b = s.islower()
+>>> print(b)
+False
+>>> b = s.index('Himanshu')
+>>> print(b)
+0
+>>> dir(list)
+['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getstate__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
