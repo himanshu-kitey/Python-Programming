0fae79b4c10a327196117b230e78ce65e905312c
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-04 02:05:44 +0530

    01_if_statement.py

---
+++ CLASS/SESSION_018/01_if_statement.py
@@ -0,0 +1,12 @@
+print('Start of Program')
+
+num = 100
+result = num
+a = int(input('Enter an integer:'))
+
+if a > 0:
+    result = num + a
+    print('Input number is positive')
+
+print('Result:', result)
+print('End of Program')
