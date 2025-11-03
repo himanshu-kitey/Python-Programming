08be4d468bd52dc5578d74b402beb48d7d972fc7
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-04 01:26:09 +0530

    01_def_statement.py

---
+++ CLASS/SESSION_015/01_def_statement.py
@@ -0,0 +1,10 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> def testFunction(a, b):
+...     print('a:', a, 'type(a):', type(a), 'id(a):', id(a))
+...     print('b:', b, 'type(b):', type(b), 'id(b):', id(b))
+... 
+...     
+>>> testFUnction(12, 12.34)
+a: 12 type(a): <class 'int'> id(a): 140736123532552
+b: 12.34 type(b): <class 'float'> id(b): 1378138329392
