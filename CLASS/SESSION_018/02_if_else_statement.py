906ffc5d747f8f2165a9046ef7216afbabd44fa2
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-04 02:09:19 +0530

    02_if_else_statement.py

---
+++ CLASS/SESSION_018/02_if_else_statement.py
@@ -0,0 +1,15 @@
+print('Start of Program')
+
+num = 100
+result = num
+a = int(input('Enter an integer:'))
+
+if a > 0:
+    print('Input number is Positive')
+    result = num / a
+else:
+    print('Input number is negative or zero')
+    result = num * a
+
+print('Result:', result)
+print('End of Program')
