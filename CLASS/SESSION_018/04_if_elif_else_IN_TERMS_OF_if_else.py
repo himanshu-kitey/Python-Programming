5303fa3d0b8b46e65868ebacc470081f6bf7e8ab
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-04 02:39:56 +0530

    04_if_elif_else_IN_TERM_OF_if_else.py

---
+++ CLASS/SESSION_018/04_if_elif_else_IN_TERMS_OF_if_else.py
@@ -0,0 +1,26 @@
+if cond:
+    BLOCK1
+else:
+    if cond1:
+        BLOCK2
+    else:
+        if cond2:
+            BLOCK3
+        else:
+            if cond3:
+                BLOCK4
+            else:
+                BLOCK5
+
+#------------------------------
+if cond:
+    BLOCK1
+elif cond1:
+    BLOCK2
+elif cond2:
+    BLOCK3
+elif cond3:
+    BLOCK4
+else:
+    BLOCK5
+#-----------------------------
