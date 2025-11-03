7154f0fdda52d34c6abc0587ac91b342176d2a20
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-04 01:30:52 +0530

    01_control_flow_with_def.py

---
+++ CLASS/SESSION_015/01_control_flow_with_def.py
@@ -0,0 +1,18 @@
+print('start of program')
+a = 10
+b = 20
+
+def testFunction(x, y):
+    print('Entered testFunction():')
+    z1 = x + y
+    z2 = x - y
+    print(z1)
+    print(z2)
+    print('Leaving testFunction():')
+
+c = a + b
+print(a)
+print(b)
+print(c)
+
+print('End of Program')
